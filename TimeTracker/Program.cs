a1d202538fbc9abe5051d8331bd8d249bb56051d
kosist <kosistivan@gmail.com>
2019-06-09 23:20:45 +0200

    Partially implemented repository for JSON file, testing

--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTracker.Data;
+using TimeTracker.RepositoriesImplementation;
 
 namespace TimeTracker
 {
@@ -10,6 +12,31 @@ namespace TimeTracker
     {
         static void Main(string[] args)
         {
+            var user1 = new User
+            {
+                Name = "John",
+                Surname = "Wick"
+            };
+
+            var user2 = new User
+            {
+                Name = "Will",
+                Surname = "Smith"
+            };
+
+            var user3 = new User
+            {
+                Name = "Bruce",
+                Surname = "Willis"
+            };
+
+            RepositoryJsonFile<User> userRepo = new RepositoryJsonFile<User>("testFile.json");
+            userRepo.Add(user1);
+            userRepo.Add(user2);
+            //Console.WriteLine(userRepo.Get(0).Name);
+            Console.WriteLine(userRepo.GetAll());
+
+
         }
     }
 }
---
+++ TimeTracker/RepositoriesImplementation/RepositoryJsonFile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TimeTracker.RepositoriesInterfaces;
+
+namespace TimeTracker.RepositoriesImplementation
+{
+    class RepositoryJsonFile<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected string FilePath;
+
+        private StreamReader OpenReader()
+        {
+            var file = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            StreamReader fileReader = new StreamReader(file);
+            return fileReader;
+        }
+        private StreamWriter OpenWriter()
+        {
+            var file = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            StreamWriter fileWriter = new StreamWriter(file);
+            return fileWriter;
+        }
+
+        public RepositoryJsonFile(string filePath)
+        {
+
+            FilePath = filePath;
+            //File = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+
+            //TODO - add handling of not valid path, etc.
+        }
+        public TEntity Get(int id)
+        {
+            IEnumerable<TEntity> records = GetAll();
+            return records.ElementAt(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            string line;
+            var records = new List<TEntity>();
+
+            JsonSerializer serializer = new JsonSerializer();
+            var fileReader = OpenReader();
+
+            using (fileReader)
+            {
+                while ((line = fileReader.ReadLine()) != null)
+                {
+                    records.Add(JsonConvert.DeserializeObject<TEntity>(line));
+                }
+            }
+            
+            return records;
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TEntity entity)
+        {
+            //IEnumerable<TEntity> records = GetAll();
+
+            //records.Append(entity);
+            
+            string json = JsonConvert.SerializeObject(entity);
+
+            var fileWriter = OpenWriter();
+
+            using (fileWriter)
+            {
+                fileWriter.WriteLine(json);
+                fileWriter.Flush();
+            }
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
