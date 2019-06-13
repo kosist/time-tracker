2402dcc249ee4efe5b92cf247c5a4ed50e41d613
kosist <kosistivan@gmail.com>
2019-06-13 20:38:46 +0200

    Implemented generic Repository for database

---
+++ TimeTracker/RepositoriesImplementation/Database/RepositoryDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.RepositoriesInterfaces;
+
+namespace TimeTracker.RepositoriesImplementation.Database
+{
+    class RepositoryDb<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        protected DbSet<TEntity> Entities;
+
+        public RepositoryDb(DbContext context)
+        {
+            Context = context;
+            Entities = Context.Set<TEntity>();
+        }
+        public TEntity Get(int id)
+        {
+            return Entities.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Entities.ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Entities.Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Entities.Remove(entity);
+        }
+    }
+}
