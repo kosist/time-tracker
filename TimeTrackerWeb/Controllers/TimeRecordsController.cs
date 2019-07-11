13e28165037d0b05307797e63eccb359bfa8c9e6
kosist <kosistivan@gmail.com>
2019-07-11 23:07:05 +0200

    Created TimeRecordViewModel, NewRecord view

---
+++ TimeTrackerWeb/Controllers/TimeRecordsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using BaseLayer.DataModels;
+using DbLayer.DbRepositories;
+using TimeTrackerWeb.Dtos;
+using TimeTrackerWeb.ViewModels;
+
+namespace TimeTrackerWeb.Controllers
+{
+    public class TimeRecordsController : Controller
+    {
+        private IUnitOfWork _context;
+        private readonly IMapper _mapper;
+
+        public TimeRecordsController(IUnitOfWork context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewRecord()
+        {
+            var users = _context.Users.GetUsers();
+            var activities = _context.LookupTables.GetActivityTypes();
+
+            var view = new TimeRecordViewModel
+            {
+                Users = _mapper.Map<IEnumerable<User>, IEnumerable<UserDto>>(users),
+                ActivityTypes = _mapper.Map<IEnumerable<ActivityType>, IEnumerable<ActivityTypeDto>>(activities),
+            };
+
+            return View(view);
+        }
+
+        public ActionResult SaveRecord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TimeTrackerWeb/ViewModels/TimeRecordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using TimeTrackerWeb.Dtos;
+
+namespace TimeTrackerWeb.ViewModels
+{
+    public class TimeRecordViewModel
+    {
+        [DisplayName("User")]
+        public IEnumerable<UserDto> Users { get; set; }
+        [DisplayName("Activity Type")]
+        public IEnumerable<ActivityTypeDto> ActivityTypes { get; set; }
+
+        public UserDto User { get; set; }
+        public ActivityTypeDto Activity { get; set; }
+
+    }
+}
